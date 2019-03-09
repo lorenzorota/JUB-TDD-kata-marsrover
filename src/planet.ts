400972cc82f7b33e618d00cd9479a0801df597d4
Lorenzo Rota <lorenzo.rota@hotmail.com>
2019-03-09 20:02:27 +0100

    Worked on so far

--- src/planet.ts
+++ src/planet.ts
@@ -3,6 +3,15 @@ export class Planet {
     obstacles: [number, number] [];
 
     constructor(size: number, obstacles: [number, number] []) {
-        
+        this.size = size;
+        this.obstacles = obstacles;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    getObstacles() {
+        return this.obstacles;
     }
 }
--- src/rover.ts
+++ src/rover.ts
@@ -5,12 +5,54 @@ export class Rover {
     y: number;
     direction: string;
     p: Planet;
+    state = {
+        "N" : {
+            "L" : ["E", 0, 0],
+            "R" : ["W", 0, 0],
+            "F" : ["N", 0, 1],
+            "B" : ["N", 0, -1]
+        },
+        "W" : {
+            "L" : ["S", 0, 0],
+            "R" : ["N", 0, 0],
+            "F" : ["W", -1, 0],
+            "B" : ["W", 1, 0]
+        },
+        "S" : {
+            "L" : ["W", 0, 0],
+            "R" : ["E", 0, 0],
+            "F" : ["S", 0, -1],
+            "B" : ["S", 0, 1]
+        },
+        "E" : {
+            "L" : ["N", 0, 0],
+            "R" : ["S", 0, 0],
+            "F" : ["E", 1, 0],
+            "B" : ["E", -1, 0]
+        }
+    };
 
     constructor(x: number, y: number, direction: string, s: number, obs: [number, number] []) {
-        
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.p = new Planet(s, obs);
     }
 
     move(instructions: string) {
-
+        for (let command of instructions) {
+            this.direction = this.state[this.direction][command][0];
+            this.x += this.state[this.direction][command][1];
+            this.y += this.state[this.direction][command][2];
+        }
     }
+
+    getDirection() {
+        return this.direction;
+    }
+
+    getCoordinates() {
+        return [this.x, this.y];
+    }
+
 }
